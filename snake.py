60213ab0a4368d9b898ff3a5ed61b42fdd18dbc3
edske1 <34068606+edske1@users.noreply.github.com>
2017-11-29 11:45:11 -0500

    Update snake.py
    add group members

--- snake.py
+++ snake.py
@@ -1,9 +1,8 @@
 '''
 Snake game.
 Authors:
-<insert the names of your teammates here>
+Edrick, Andrew, Mardokai
 '''
-[Ej, Andrew, Mardokai]
 import pygame
 import random
 import sys
@@ -55,7 +54,7 @@ def create_food_position():
     The first element is the x position. Must be an int between 0 and GRID_WIDTH - 1, inclusively.
     The second element is the y position. Must be an int between 0 and GRID_HEIGHT - 1, inclusively.
     """
-    return
+    return (12, 12)
 
 def snake_ate_food(snake, food):
     """Returns whether food was eaten by the snake.
